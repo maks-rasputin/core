a1e9e301bc11b036aede0abffc214f0bb1487e40
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-05-09 20:35:51 +0000

    Add Zcash support to Thorchain swaps

--- crates/primitives/src/wallet_configuration.rs
+++ crates/primitives/src/wallet_configuration.rs
@@ -1,14 +1,14 @@
 use serde::{Deserialize, Serialize};
 use typeshare::typeshare;
 
-use crate::WalletId;
+use crate::{ChainAddress, WalletId};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 #[typeshare(swift = "Equatable, Sendable")]
 #[serde(rename_all = "camelCase")]
 pub struct WalletConfiguration {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub has_multi_signature_accounts: Option<bool>,
+    #[serde(default)]
+    pub multi_signature_accounts: Vec<ChainAddress>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
